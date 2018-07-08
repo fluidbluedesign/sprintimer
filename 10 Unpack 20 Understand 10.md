6a5399f21e19a9c4a4fbe61e45cb07d8b97494ac
thejus11 <thejus11@gmail.com>
2018-07-08 17:32:20 +0530

    Started timer with some seconds

---
+++ 10 Unpack 20 Understand 10.md
@@ -0,0 +1,12 @@
+10 Unpack
+20 Understand
+10 Synthesize
+20 Ideate
+20 Prototype 
+20 Refine 
+
+Total - 1 hour 40 minutes ie 100 Minutes
+
+Therefore
+
+Multiply by 0.1 or 0.2 depending
---
+++ firstScript.js
@@ -0,0 +1,71 @@
+//get value for unpack from localstorage and convert into Integer
+
+var unpackValue = localStorage.getItem("unpackValue");
+console.log(unpackValue);
+timerValue = parseInt(unpackValue);
+var timerValue = 60;
+
+
+
+
+//variables for progress bar
+var progBarPerc = 0;
+var currentTicker = 1;
+var actualBar = document.getElementById("progressBar");
+var totalTimeCount = document.getElementById("totalTime");
+
+//first timer
+
+//assign firsttimerseconds to current timer
+var firstTimer = new Timer();
+firstTimer.start({precision: 'seconds', countdown: true, startValues: {seconds: timerValue}});
+$('#timerContainer #timer').html(firstTimer.getTimeValues().toString());
+firstTimer.addEventListener('secondsUpdated', function (e) {
+    $('#timerContainer #timer').html(firstTimer.getTimeValues().toString());
+});
+
+firstTimer.addEventListener('secondTenthsUpdated', function (e) {
+    //runs the progress bar updater every second
+    progressBarUpdater();
+});
+
+firstTimer.addEventListener('targetAchieved', function (e) {
+    //reset the currentTicker and update the progress bar
+    currentTicker = 0;
+    progressBarUpdater();
+    launchSecTimer();
+    //launch notification sound
+    audioAlert();
+});
+
+$('#pauseButton').click(function () {
+    firstTimer.pause();
+});
+
+$('#skipAheadButton').click(function () {
+    firstTimer.pause();
+    launchSecTimer();
+});
+
+
+
+
+//updates progress bar
+function progressBarUpdater(){
+    
+    //updates currentTicker every second
+    if (currentTicker < timerValue){
+    currentTicker = currentTicker + 0.1;
+    }
+    
+    var progBarPerc = (currentTicker/timerValue)*100;
+    actualBar.style.width = progBarPerc + '%';
+
+}
+
+function audioAlert(){
+var audio = new Audio('notification.wav');
+audio.play();
+    
+}
+
